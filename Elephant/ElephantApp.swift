e56ab37564e3ca3af2b70d0e9ac9de0b4f7c8586
candicehclu <candicehclu@gmail.com>
2025-02-16 16:52:03 -0600

    added new project files

---
+++ Elephant/ElephantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElephantApp.swift
+//  Elephant
+//
+//  Created by 陸卉媫 on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ElephantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
