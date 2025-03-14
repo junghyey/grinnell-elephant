13a4b4a0a2ca0556aca8ecff0a32ac0d7907c9af
candicehclu <candicehclu@gmail.com>
2025-03-14 05:09:17 -0500

    Minor fix in ElephantApp

--- Elephant/ElephantApp.swift
+++ Elephant/ElephantApp.swift
@@ -14,6 +14,7 @@ struct ElephantApp: App {
             ContentView()
             SettingsView()
             ShopMainPageView()
+            ItemView()
         }
         .windowResizability(.contentSize)
         .defaultSize(width: 400, height: 600)
