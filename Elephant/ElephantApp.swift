60f3702cadac819e1e129c1b4039a6a7d7a250ef
Gabriela Pruneda Turcios <gabrielapruneda2003@gmail.com>
2025-04-10 10:53:50 -0500

    Update ElephantApp.swift

--- Elephant/ElephantApp.swift
+++ Elephant/ElephantApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct ElephantApp: App {
+    @StateObject private var taskListStorage = TaskListStorage()
+    @StateObject private var settingsModel = SettingsModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(taskListStorage)//defines storage for task list upon app build
+                .environmentObject(settingsModel)//sets settings mode and theme background upon app build
         }
         .windowResizability(.contentSize)
         .defaultSize(width: 500, height: 500)
