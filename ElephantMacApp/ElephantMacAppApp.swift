01355fe5adda63b436417af5dfe08c7ab319eb6f
candicehclu <candicehclu@gmail.com>
2025-04-13 21:10:54 -0500

    Added ElephantMacApp and configured widget to run under the macOS app

---
+++ ElephantMacApp/ElephantMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElephantMacAppApp.swift
+//  ElephantMacApp
+//
+//  Created by 陸卉媫 on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ElephantMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
