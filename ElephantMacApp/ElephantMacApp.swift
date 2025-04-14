c25bf842b723c02b4bc72c630b13fd581383a40f
candicehclu <candicehclu@gmail.com>
2025-04-14 11:10:44 -0500

    Migrated files to mac app

---
+++ ElephantMacApp/ElephantMacApp.swift
@@ -0,0 +1,19 @@
+//
+//  ElephantMacApp.swift
+//  Elephant
+//
+//  Created by 陸卉媫 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ElephantMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowResizability(.contentSize)
+        .defaultSize(width: 500, height: 500)
+    }
+}
